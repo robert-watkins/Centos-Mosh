6db6c2ecb03a1c0d349f24fa4e02b23086c97199
Robert Watkins <4706327+robert-watkins@users.noreply.github.com>
2020-03-28 20:36:53 -0600

    iptables --> firewall-cmd
    iptables doesn't like to save for me on cent 8

--- setup.sh
+++ setup.sh
@@ -8,10 +8,11 @@ fi
 echo Installing Mosh...
 dnf install epel-release -yq
 dnf install mosh -yq
-iptables -I INPUT 1 -p udp --dport 60000:61000 -j ACCEPT
-service iptables save
+firewall-cmd --zone=public --permanent --add-port=60000-61000/udp
 for user in /home/*/; do 
     echo "LANG=\"en_US.UTF-8\"" >> $user/.bashrc
     echo "export LANG" >> $user/.bashrc
 done
 echo Done
+systemctl reload firewalld
+exit 0
